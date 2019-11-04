a331ac7f722b01cb2cdaa627b5216c249da1c083
Milena <MilenaPechura@gmail.com>
2019-11-04 14:41:01 +0200

    removed unnecessary files and lines

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,6 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
-function style(){
-    return gulp.src('./scss/**/*.scss')
-        .pipe(sass())
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream())
-}
 
 function watch(){
     browserSync.init({
@@ -17,9 +8,7 @@ function watch(){
             baseDir:'./'
         }
     });
-    gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload)
 }
 
-exports.style = style;
 exports.watch = watch;
